f276a1ff267ae9cb1aa31754b7bd967b81726ac6
zhuzhu <zz_hh_uu@163.com>
2022-08-31 22:33:46 +0800

    write file

---
+++ file/write/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func write1() {
+	filePath := "/data/abc.txt"
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Printf("open file err=%v\n", err)
+		return
+	}
+	defer file.Close()
+	str := "hello world\n"
+	//写入时。使用带缓存的*Writer
+	writer := bufio.NewWriter(file)
+	for i := 0; i < 5; i++ {
+		writer.WriteString(str)
+	}
+	writer.Flush() //将缓冲的数据真正写入到文件中
+}
+
+func main() {
+	write1()
+}
