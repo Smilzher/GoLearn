e70b63b98976f1798bc8b27570e0c1dbe3d7bef8
zhuzhu <zz_hh_uu@163.com>
2022-09-01 11:50:14 +0800

    file copy

---
+++ file/copy/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func CopyFile(dstFileName string, srcFileName string) (written int64, err error) {
+	srcFile, err := os.Open(srcFileName)
+	if err != nil {
+		fmt.Printf("open file err=%v\n", err)
+	}
+	defer srcFile.Close()
+	reader := bufio.NewReader(srcFile)
+
+	distFile, err := os.OpenFile(dstFileName, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Printf("open file err=%v\n", err)
+		return
+	}
+	defer distFile.Close()
+
+	writer := bufio.NewWriter(distFile)
+	return io.Copy(writer, reader)
+}
+
+func main() {
+	srcFile := "/data/P26.jpg"
+	dstFile := "/data/test/test.jpg"
+	_, err := CopyFile(dstFile, srcFile)
+	if err == nil {
+		fmt.Printf("拷贝完成\n")
+	} else {
+		fmt.Printf("拷贝错误 err=%v\n", err)
+	}
+}
