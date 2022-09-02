30c8840463f9ddb639c523b63919362c446835d1
zhuzhu <zz_hh_uu@163.com>
2022-09-02 16:40:18 +0800

    单元测试

---
+++ test/cal_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestGetSum(t *testing.T) {
+	res := add(10)
+	if res != 55 {
+		t.Fatalf("addUpper(10) is 55, error res=%v", res)
+	}
+	t.Logf("test GetSum success")
+}
