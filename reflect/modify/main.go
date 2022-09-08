b4aa7c4a14aadf9c349794f927b4617d220d2009
zhuzhu <zz_hh_uu@163.com>
2022-09-08 15:57:24 +0800

    反射实例

--- reflect/modify/main.go
+++ reflect/modify/main.go
@@ -12,8 +12,21 @@ func testInt(b interface{}) {
 	fmt.Printf("val=%v\n", val)
 }
 
+func testFloat(b interface{}) {
+	rTyp := reflect.TypeOf(b)
+	val := reflect.ValueOf(b)
+	fmt.Println("rTyp=", rTyp)
+	fmt.Println("kind=", val.Kind())
+	iVal := val.Interface()
+	floatVal := iVal.(float64)
+	fmt.Printf("floatVal=%v, Type=%T\n", iVal, floatVal)
+}
+
 func main() {
 	var num int = 20
 	testInt(&num)
 	fmt.Println("num=", num)
+	fmt.Println()
+	var num2 float64 = 1.2
+	testFloat(num2)
 }
