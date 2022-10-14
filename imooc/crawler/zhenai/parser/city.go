19740b4b6c830fe239934587e3d32ec0c9ee452c
zhuzhu <zz_hh_uu@163.com>
2022-10-14 10:59:05 +0800

    城市解析器

--- imooc/crawler/zhenai/parser/city.go
+++ imooc/crawler/zhenai/parser/city.go
@@ -5,19 +5,19 @@ import (
 	"regexp"
 )
 
-const cityListRe = `<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]*)</a>`
+const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]*)</a>`
 
-func ParseCityList(contents []byte) engine.ParseResult {
-	re := regexp.MustCompile(cityListRe)
+func ParseCity(contents []byte) engine.ParseResult {
+	re := regexp.MustCompile(cityRe)
 	//matches := re.FindAll(contents, -1)
 	matches := re.FindAllSubmatch(contents, -1) //有括号匹配出内容时，使用findAllSubmatch
 
 	result := engine.ParseResult{}
 	for _, m := range matches {
-		result.Items = append(result.Items, "City "+string(m[2]))
+		result.Items = append(result.Items, "User "+string(m[2]))
 		result.Requests = append(result.Requests, engine.Request{
 			Url:        string(m[1]),
-			ParserFunc: ParseCity,
+			ParserFunc: engine.NilParser,
 		})
 		//fmt.Printf("City: %s, URL: %s \n", m[2], m[1])
 	}
