49e71ef98db21a985380c9f7a3177ae446c462a1
zhuzhu <zz_hh_uu@163.com>
2022-10-17 22:09:33 +0800

    面试题

---
+++ interview/ret/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func b() (i int) {
+	defer func() {
+		i++
+		fmt.Println("defer2:", i)
+	}()
+	defer func() {
+		i++
+		fmt.Println("defer1:", i)
+	}()
+	return i //或者直接写成return
+}
+
+func c() *int {
+	var i int
+	defer func() {
+		i++
+		fmt.Println("defer2:", i)
+	}()
+	defer func() {
+		i++
+		fmt.Println("defer1:", i)
+	}()
+	return &i
+}
+
+func main() {
+	fmt.Println("return b():", b())
+	fmt.Println("return c():", *(c()))
+}
